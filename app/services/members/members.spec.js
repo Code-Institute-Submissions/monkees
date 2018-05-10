ae580479fba76f1cc2390eb0689c7100e5efcc48
daniel angelo deiana <daniel@daniels-MacBook-Air.local>
2018-05-10 09:59:49 +0100

    added artist info to members service

--- app/services/members/members.spec.js
+++ app/services/members/members.spec.js
@@ -5,8 +5,45 @@ describe('Members factory', function(){
 
     var membersList = [
         {
-            name: 'Peter tork',
-        }
+            name: 'Peter Tork',
+            born: '13 February 1942',
+            instruments: [
+                'bass guitar',
+                'vocals',
+                'keyboards',
+            ],
+            website: 'https://www.petertork.com',
+            wikipedia: 'https://en.wikipedia.org/wiki/Peter_Tork',
+        },
+        {
+            name: 'Micky Dolenz',
+            born: '8 March 1945',
+            instruments: [
+                'drums',
+                'vocals',
+            ],
+            website: 'https://www.mickydolenz.com',
+            wikipedia: 'https://en.wikipedia.org/wiki/Micky_Dolenz',
+        },
+        {
+            name: 'Davy Jones',
+            born: '30 December 1945',
+            instruments: [
+                'vocals',
+            ],
+            website: 'https://www.davyjones.net',
+            wikipedia: 'https://en.wikipedia.org/wiki/Davy_Jones_(musician)',
+        },
+        {
+            name: 'Michael Nesmith',
+            born: '30 December 1942',
+            instruments: [
+                'guitar',
+                'vocals',
+            ],
+            website: undefined,
+            wikipedia: 'https://en.wikipedia.org/wiki/Peter_Tork',
+        },
     ]
 
     // load api.members module
